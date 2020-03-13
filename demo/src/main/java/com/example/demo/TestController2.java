c9b8f8f28d86c9619cea83db91c4cb37ff78e7a0
stevebank1911 <57536883+stevebank1911@users.noreply.github.com>
2020-03-14 07:27:35 +0900

    second commit

---
+++ demo/src/main/java/com/example/demo/TestController2.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.InfoDao;
+
+@Controller
+public class TestController2 {
+	
+	@Autowired
+	InfoDao infodao;
+	
+	@RequestMapping("/testjsp")
+	public String root() throws Exception {
+		
+		int testno = infodao.test();
+		System.out.println("testno:" + testno);
+		
+		return "testjsp";
+	}
+
+}
